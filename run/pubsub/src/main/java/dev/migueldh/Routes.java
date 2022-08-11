2fa4baed60284d3dc5590000983892b0d6ab4191
[USERNAME] <student-04-c24274bd7744@qwiklabs.net>
2022-08-11 14:20:08 -0500

    Camel route modified with Exceptions and handle Empty messages

--- run/pubsub/src/main/java/dev/migueldh/Routes.java
+++ run/pubsub/src/main/java/dev/migueldh/Routes.java
@@ -1,6 +1,7 @@
 package dev.migueldh;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 
 public class Routes extends RouteBuilder {
@@ -12,15 +13,33 @@ public class Routes extends RouteBuilder {
 
         rest("/")
          .post()
-         .to("direct:printMessage");
+         .to("direct:processMessage");
 
+         onException(Exception.class)
+         .handled(true)
+         .log("Server Error")
+         .setBody(simple("Internal Server Error"));
+
+
+        from("direct:processMessage")
+         .marshal().json(JsonLibrary.Jackson)
+        .choice()
+          .when().jsonpath("$.message.data",true)
+            .to("direct:printMessage")
+          .otherwise() 
+            .to("direct:printEmptyMessage");
 
         from("direct:printMessage")
          .transform().jsonpath("$.message.data")
          .unmarshal().base64()
-        .log("Hello ${body}!");
+         .log("Hello ${body}!")
+         .setBody(simple("Information Received"));
 
-         
+         from("direct:printEmptyMessage")
+          .log("Hello World!")
+          .setBody(simple("Information Received was empty"));
+
+
     }
     
 }
